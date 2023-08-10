473f01e39f9c3d051be7d3ee4d7ecac0390878f5
Emblen <89460730+Kemblen@users.noreply.github.com>
2023-08-11 00:37:03 +0900

    first commit

---
+++ AHC022.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <fstream>
+#define INF 1e9
+#define vi vector<int>
+#define vb vector<bool>
+#define vll vector<long long>
+#define v2i vector<vector<int>>
+#define v2b vector<vector<bool>>
+#define all(x) x.begin(),x.end()
+using namespace std;
+using ll = long long;
+
+struct vec2{
+    int y, x; 
+    bool operator<(const vec2 right){
+        return y == right.y ? x < right.x : y < right.y;
+    }
+};
+
+struct Solver{
+    
+
+    Solver solver()
+    {}
+
+    void solve(){
+
+
+        return ;
+    }
+};
+
+
+
+int main(){
+
+    return 0;
+}
