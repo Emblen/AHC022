6a74532dbb171df33835b3407204f36288938b83
Emblen <89460730+Kemblen@users.noreply.github.com>
2023-08-15 14:15:52 +0900

    σの値で初期温度とthresholdを調整

--- AHC022.cpp
+++ AHC022.cpp
@@ -24,6 +24,9 @@ const int max_measure = 10000;
 const string inputfile = "input.txt";
 const string outputfile = "output.txt";
 
+const vi temp_vec = {400, 400, 500, 800, 800, 1000, 1000, 1000, 1000, 1000};
+const vi threshold_vec = {200, 200, 350, 600, 600, 800, 800, 800, 800, 800};
+
 struct vec2{
     int y, x; 
     bool operator<(const vec2 right) const {
@@ -136,27 +139,32 @@ struct Solver{
 
     v2i create_temperature(){
         v2i temperature(L, vi(L, 0));
+        int temp = temp_vec[S/100];
         for (int i = 0; i < N; i++) {
-            temperature[exit_cell[i].y][exit_cell[i].x] = 800;
+            temperature[exit_cell[i].y][exit_cell[i].x] = temp;
         }
         return temperature;
     }
     
     vi predict(const v2i& temperature, vector<vector<vec2>> neighbor){
-        set<vec2> exit_set;
-        for(int i=0; i<N; i++) exit_set.insert(exit_cell[i]);
+        vector<vec2> exit_vec;
+        for(int i=0; i<N; i++) exit_vec.push_back(exit_cell[i]);
+
         map<vec2, int> exit_map;
         for(int i=0; i<N; i++) exit_map.insert({exit_cell[i], i});
 
         vi estimate(N, 0);
-        const int threshold = 600;
+        const int threshold = threshold_vec[S/100];
         int measure_cnt = 0;
+        int last_i = -1;
 
         for(int i=0; i<N; i++){
             int mxcnt = -1, mxj = -1;
             bool assigned = false;
 
-            for(auto cell:exit_set){
+            int cellcnt = -1;
+            for(auto cell:exit_vec){ 
+                cellcnt++;
                 vec2 base = cell;
 
                 bool flag = true;
@@ -182,7 +190,13 @@ struct Solver{
                 if(flag){
                     assigned = true;
                     estimate[i] = j; 
-                    exit_set.erase(cell);
+                    exit_vec.erase(exit_vec.begin()+cellcnt);
+                    break;
+                }
+                if(S>500 && truecnt>=3){
+                    assigned = true;
+                    estimate[i] = j; 
+                    exit_vec.erase(exit_vec.begin()+cellcnt);
                     break;
                 }
                 //フラグがfalseなら、一部または全部出口セルでなかったので出口セルだった数とjの値を記録しておく
@@ -192,8 +206,15 @@ struct Solver{
                 }
             }
 
-            if(measure_cnt>=max_measure) break;
+            if(measure_cnt>=max_measure) {last_i = i; break;}
             if(!assigned) estimate[i] = mxj; //一番多く出口セルだったjを予測値として採用する
+        }
+        if(measure_cnt>=max_measure){
+            for(auto cell:exit_vec){
+                estimate[last_i] = exit_map[cell];
+                last_i++;
+                if(last_i==N) break;
+            }
         }
         return estimate;
     }
@@ -231,7 +252,7 @@ struct LocalJudge {
 
         ofstream output;
         output.open(outputfile, ios::app);
-        output << "# return value = " << v << endl;
+        // output << "# return value = " << v << endl;
         output << i << " " << y << " " << x << endl;
         output.close();
 
@@ -276,13 +297,10 @@ struct LocalSolver{
 
     v2i create_temperature(){
         v2i temperature(L, vi(L, 0));
-        /////////////ヒートマップを決める
-
-        for (int i = 0; i < N; i++) {   
-            temperature[exit_cell[i].y][exit_cell[i].x] = 800;
+        int temp = temp_vec[S/100];
+        for (int i = 0; i < N; i++) {
+            temperature[exit_cell[i].y][exit_cell[i].x] = temp;
         }
-
-        ////////////
         return temperature;
     }
 
@@ -298,7 +316,7 @@ struct LocalSolver{
     vector<vector<vec2>> BFS(v2b exit_cell_map){
         vector<vector<vec2>> neighbor(N);
         const vector<vec2> dydx = {{1,0},{0,1},{-1,0},{0,-1}};
-        const int nb = 2;//近傍の数
+        const int nb = 4;//近傍の数
 
         for(int i=0; i<N; i++){
             vec2 cell = exit_cell[i];
@@ -334,13 +352,13 @@ struct LocalSolver{
             }
         }
 
-        for(int i=0; i<N; i++){
-            cout << "# (" << exit_cell[i].y << "," << exit_cell[i].x << ")" << ": ";
-            for(int j=0; j<nb; j++){
-                cout << "(" << neighbor[i][j].y << "," << neighbor[i][j].x << ")" << " ";
-            }
-            cout << endl;
-        }
+        // for(int i=0; i<N; i++){
+        //     cout << "# (" << exit_cell[i].y << "," << exit_cell[i].x << ")" << ": ";
+        //     for(int j=0; j<nb; j++){
+        //         cout << "(" << neighbor[i][j].y << "," << neighbor[i][j].x << ")" << " ";
+        //     }
+        //     cout << endl;
+        // }
         return neighbor;
     }
 
@@ -349,14 +367,16 @@ struct LocalSolver{
 
     vi predict(const v2i& temperature, vector<vector<vec2>> neighbor){
 
-        set<vec2> exit_set;
-        for(int i=0; i<N; i++) exit_set.insert(exit_cell[i]);
+        vector<vec2> exit_vec;
+        for(int i=0; i<N; i++) exit_vec.push_back(exit_cell[i]);
         //該当出口セルが見つかったらeraseする。これで計算量減らせるね。
         map<vec2, int> exit_map;
         for(int i=0; i<N; i++) exit_map.insert({exit_cell[i], i});
 
         vi estimate(N, 0);
-        const int threshold = 600;//σと初期ヒートマップの値によって可変にしたら良さそう
+        const int threshold = threshold_vec[S/100];//σと初期ヒートマップの値によって可変にしたら良さそう
+        int measure_cnt = 0;
+        int last_i = -1;
 
         //全てのワームホールについて、ある出口セルとその近傍の2つの出口セルの差分を出力し、温度の計測を行うことで出口セルかどうか判別する。
         for(int i=0; i<N; i++){
@@ -365,7 +385,10 @@ struct LocalSolver{
             vec2 ansi_pos = exit_cell[ans[i]];
 
             // cout << (int)exit_set.size() << " ";
-            for(auto cell:exit_set){ //近傍の出口セルを計測先として指定し、そこが出口セルかどうか見極める。これによって元の出口セルが判別できる。
+            int cellcnt = -1;
+            for(auto cell:exit_vec){ //近傍の出口セルを計測先として指定し、そこが出口セルかどうか見極める。これによって元の出口セルが判別できる。
+                cellcnt++;
+            //1回ごとにvecをシャッフルしたほうが良さそう？
                 vec2 base = cell;
 
                 bool flag = true;
@@ -376,29 +399,40 @@ struct LocalSolver{
                     int dy = nb.y - base.y;
                     int dx = nb.x - base.x;
 
-                    ofstream output;
-                    output.open(outputfile, ios::app);
-                    output << "# measure i=" << i << " y=" << dy << " x=" << dx << endl;
+                    // ofstream output;
+                    // output.open(outputfile, ios::app);
+                    // output << "# measure i=" << i << " y=" << dy << " x=" << dx << endl;
                     
                     
                     
                     vec2 mp = {ansi_pos.y+dy, ansi_pos.x+dx};
                     if(mp.y<0 || mp.y>=L || mp.x<0 || mp.x>=L) {flag = false; continue;}
-                    output << "# mp_pos = ("<<mp.y << ","<<mp.x<<")" << endl;
-                    output.close();
+                    // output << "# mp_pos = ("<<mp.y << ","<<mp.x<<")" << endl;
+                    // output.close();
 
                     int pval = temperature[mp.y][mp.x];
                     int v = localjudge.measure(i, dy, dx, pval);
+
+                    measure_cnt++;
+                    if(measure_cnt>=max_measure) break;
 
                     //出口セルか否かここで判断する
                     if(v < threshold) flag = false;
                     else truecnt++;
                 }
+                if(measure_cnt>=max_measure) break;
+
                 //フラグがtrueなら全てが出口セルだったので元の出口セルを予測値とする
                 if(flag){
                     assigned = true;
                     estimate[i] = j; 
-                    exit_set.erase(cell);
+                    exit_vec.erase(exit_vec.begin()+cellcnt);
+                    break;
+                }
+                if(S>500 && truecnt>=3){
+                    assigned = true;
+                    estimate[i] = j; 
+                    exit_vec.erase(exit_vec.begin()+cellcnt);
                     break;
                 }
                 //フラグがfalseなら、一部または全部出口セルでなかったので出口セルだった数とjの値を記録しておく
@@ -407,7 +441,16 @@ struct LocalSolver{
                     mxj = j;
                 }
             }
+            if(measure_cnt>=max_measure) {last_i = i; break;}
+
             if(!assigned) estimate[i] = mxj; //一番多く出口セルだったjを予測値として採用する
+        }
+        if(measure_cnt>=max_measure){
+            for(auto cell:exit_vec){
+                estimate[last_i] = exit_map[cell];
+                last_i++;
+                if(last_i==N) break;
+            }
         }
         return estimate;
     }
@@ -439,6 +482,6 @@ int main(){
     // LocalSolver localsolver(l, n, s, exit_cell, ans, f);
     // localsolver.solve();
 
-    // cout << "# completed successfully" << endl;
+    cout << "# completed successfully" << endl;
     return 0;
 }
