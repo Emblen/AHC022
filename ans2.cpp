d80caee7d91497402c347c87a08ba7a748b2350b
Emblen <89460730+Kemblen@users.noreply.github.com>
2023-08-18 15:48:46 +0900

    パラメータを雑に調整

--- ans2.cpp
+++ ans2.cpp
@@ -109,8 +109,8 @@ struct Solver{
     //ヒートマップを生成する
     v2i create_temperature(vec2 pivot){
         v2i temperature(L, vi(L, 0));
-        // int temp = (S<40) ? 400 : min((int)((double)S*2.4), 1000);
-        int temp = 1000;
+        int temp = (S<40) ? 400 : max(min(400,(int)((double)S*2.4)), 1000);
+        // int temp = 1000;
 
         temperature[pivot.y][pivot.x] = temp;
         return temperature;
@@ -138,8 +138,8 @@ struct Solver{
 
     vi predict(v2i temperature, vector<vec2> diff_vec){
         vi estimate(N, 0);
-        // int threshold = (S<40) ? 200 : min((int)((double)S*2.4), 1000)/2*3;  //2で割るだけだとやばそう
-        int threshold = 800;
+        int threshold = (S<40) ? 200 : max(200, min((int)((double)S*2.4)*3, 1000));  //2で割るだけだとやばそう
+        if(threshold==1000) threshold -= S/4;
         cout << "# threshold = " << threshold << endl;
         
         map<vec2, int> diff_exitcellnum;
@@ -277,8 +277,9 @@ struct LocalSolver{
     //ヒートマップを生成する
     v2i create_temperature(vec2 pivot){
         v2i temperature(L, vi(L, 0));
-        // int temp = (S<40) ? 400 : min((int)((double)S*2.4), 1000);
-        int temp = init_tmp;
+        int temp = (S<40) ? 400 : max(min(400,(int)((double)S*2.4)), 1000);
+        cout << "# temperature = " << temp << endl;
+        // int temp = init_tmp;
 
         temperature[pivot.y][pivot.x] = temp;
         return temperature;
@@ -306,8 +307,11 @@ struct LocalSolver{
 
     vi predict(v2i temperature, vector<vec2> diff_vec){
         vi estimate(N, 0);
-        // int threshold = (S<40) ? 200 : min((int)((double)S*2.4), 1000)/2*3;  //2で割るだけだとやばそう
-        int threshold = init_threshold;
+        int threshold = (S<40) ? 200 : max(200, min((int)((double)S*1.2), 1000));  //2で割るだけだとやばそう
+        if(threshold==1000) threshold -= S/4;
+        // int threshold = init_threshold;
+        // int threshold = 900;
+        
         cout << "# threshold = " << threshold << endl;
         // 0. diff_vecと出口セルの番号を対応させるmapを作成
         // 1. diff_vecをシャッフル
@@ -359,28 +363,28 @@ struct LocalSolver{
 
 int main(){
 //Submit
-    // int l, n, s; cin >> l >> n >> s;
-    // vector<vec2> exit_cell(n);
-    // //全て異なり、辞書順に与えられる
-    // for(int i=0; i<n; i++) cin >> exit_cell[i].y >> exit_cell[i].x;
+    int l, n, s; cin >> l >> n >> s;
+    vector<vec2> exit_cell(n);
+    //全て異なり、辞書順に与えられる
+    for(int i=0; i<n; i++) cin >> exit_cell[i].y >> exit_cell[i].x;
 
-    // Solver solver(l, n, s, exit_cell);
-    // solver.solve();
+    Solver solver(l, n, s, exit_cell);
+    solver.solve();
 
 //Local
-    ifstream input(inputfile);
-    int l, n, s;
-    vector<vec2> exit_cell(n);
-    vi ans(n), f(max_measure);
+    // ifstream input(inputfile);
+    // int l, n, s;
+    // vector<vec2> exit_cell(n);
+    // vi ans(n), f(max_measure);
 
-    input >> l >> n >> s;
-    for(int i=0; i<n; i++) input >> exit_cell[i].y >> exit_cell[i].x;
-    for(int i=0; i<n; i++) input >> ans[i];
-    for(int i=0; i<max_measure; i++) input >> f[i];
-    input.close();
+    // input >> l >> n >> s;
+    // for(int i=0; i<n; i++) input >> exit_cell[i].y >> exit_cell[i].x;
+    // for(int i=0; i<n; i++) input >> ans[i];
+    // for(int i=0; i<max_measure; i++) input >> f[i];
+    // input.close();
 
-    LocalSolver localsolver(l, n, s, exit_cell, ans, f);
-    localsolver.solve();
+    // LocalSolver localsolver(l, n, s, exit_cell, ans, f);
+    // localsolver.solve();
 
     cout << "# completed successfully" << endl;
     return 0;
